5f330586a3546ef4773aba0af3a4fa0b2e0ad776
NDlinh-ndl <linhduc159@gmail.com>
2024-10-19 11:58:35 +0700

    Create ChiTietDonHang.cs

---
+++ LNBT/ViewModel/ChiTietDonHang.cs
@@ -0,0 +1,27 @@
+﻿using LNBT.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LNBT.ViewModel
+{
+    internal class ChiTietDonHang
+    {
+        public int MaChiTietDonHang { get; set; }
+
+        public int? MaDonHang { get; set; }
+
+        public int? MaSanPham { get; set; }
+
+        public int SoLuong { get; set; }
+
+        public decimal Gia { get; set; }
+        public decimal? ThanhTien { get; set; }
+
+        public virtual DonHang DonHang { get; set; }
+
+        public virtual SanPham SanPham { get; set; }
+    }
+}
